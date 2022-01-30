5126e2f2b5e5255155f29c802841681912fe8ffd
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-01-31 00:04:38 +0100

    Add compat for purrfied water & biomesoplenty aquatic plants

--- src/main/java/com/charles445/simpledifficulty/block/BlockFluidBasic.java
+++ src/main/java/com/charles445/simpledifficulty/block/BlockFluidBasic.java
@@ -9,8 +9,12 @@ import com.google.common.collect.Maps;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fml.common.Loader;
@@ -26,17 +30,22 @@ public class BlockFluidBasic extends BlockFluidClassic
 		setUnlocalizedName(this.getRegistryName().toString());
 		SDFluids.fluidBlocks.put(fluid.getName(), this);
 
-		if (Loader.isModLoaded("backportedflora")) {
-			displacements.putAll(customDisplacements);
-		}
+		displacements.putAll(customDisplacements);
 	}
 
 	protected final static Map<Block, Boolean> customDisplacements = Maps.newHashMap();
 	static
 	{
-		customDisplacements.put(REGISTRY.getObject(new ResourceLocation("backportedflora", "rivergrass")), false);
-		customDisplacements.put(REGISTRY.getObject(new ResourceLocation("backportedflora", "seagrass")), false);
-		customDisplacements.put(REGISTRY.getObject(new ResourceLocation("backportedflora", "kelp")), false);
+		if (Loader.isModLoaded("biomesoplenty")) {
+			customDisplacements.put(REGISTRY.getObject(new ResourceLocation("biomesoplenty", "coral")), false);
+			customDisplacements.put(REGISTRY.getObject(new ResourceLocation("biomesoplenty", "seaweed")), false);
+		}
+		if (Loader.isModLoaded("backportedflora")) {
+			customDisplacements.put(REGISTRY.getObject(new ResourceLocation("backportedflora", "rivergrass")), false);
+			customDisplacements.put(REGISTRY.getObject(new ResourceLocation("backportedflora", "seagrass")), false);
+			customDisplacements.put(REGISTRY.getObject(new ResourceLocation("backportedflora", "kelp")), false);
+		}
+
 		customDisplacements.put(Blocks.WATER, false);
 	}
 
