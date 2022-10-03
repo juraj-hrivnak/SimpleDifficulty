fc6818e36bdf1ce928bd9e2854a3cdef3b6e27f4
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2022-10-03 15:20:48 +0200

    Bump version

--- src/main/java/com/charles445/simpledifficulty/SimpleDifficulty.java
+++ src/main/java/com/charles445/simpledifficulty/SimpleDifficulty.java
@@ -27,7 +27,7 @@ public class SimpleDifficulty
 {
 	public static final String MODID = "simpledifficulty";
 	public static final String NAME = "SimpleDifficulty";
-	public static final String VERSION = "0.5.4";
+	public static final String VERSION = "0.5.5";
 	
 	@Mod.Instance(SimpleDifficulty.MODID)
 	public static SimpleDifficulty instance;
